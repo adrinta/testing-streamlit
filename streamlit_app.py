ac2d3cc9745d05111ea5a8427f6defd8624d2c79
Muhammad Adrinta Abdurrazzaq <adrintaabdurrazzaq@gmail.com>
2023-12-15 10:06:28 +0700

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,22 @@ import requests
 x = requests.get('https://w3schools.com/python/demopage.htm')
 
 #print the response text (the content of the requested file):
-st.write(x.text)
 
 
 st.title("Streamlit Dashboard Demo")
 
-print("test")
+def my_widget(key):
+    st.subheader('Hello there!')
+    return st.button("Click me " + key)
+
+# This works in the main area
+clicked = my_widget("first")
+
+# And within an expander
+my_expander = st.expander("Expand", expanded=True)
+with my_expander:
+    clicked = my_widget("second")
+
+# AND in st.sidebar!
+with st.sidebar:
+    clicked = my_widget("third")
