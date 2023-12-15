fcc698b7220b80a4a2c52081dabb019b95e7d536
Muhammad Adrinta Abdurrazzaq <adrintaabdurrazzaq@gmail.com>
2023-12-15 09:54:27 +0700

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import requests
+
+#the required first parameter of the 'get' method is the 'url':
+x = requests.get('https://w3schools.com/python/demopage.htm')
+
+#print the response text (the content of the requested file):
+st.write(x.text)
+
 
 st.title("Streamlit Dashboard Demo")
 
